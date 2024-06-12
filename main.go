ff210e8177029cbd516b33f6728f5df87e2e4df7
Alessio Biancalana <alessio@dottorblaster.it>
2024-06-12 12:22:39 +0200

    Add a config PoC

--- main.go
+++ main.go
@@ -20,8 +20,12 @@ type FooResponse struct {
 	Bar string
 }
 
+type Config struct {
+  Request Request[]
+}
+
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "usage:", os.Args[0], "/path.sock")
 		return
 	}
