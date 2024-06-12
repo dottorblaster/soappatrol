b578bf57d335827ed56bd089d38762d31e1df0d5
Alessio Biancalana <alessio.biancalana@suse.com>
2024-06-12 18:06:02 +0200

    Bind handlers to requests in config

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net"
 	"net/http"
 	"os"
+
+	"github.com/BurntSushi/toml"
 )
 
 // FooRequest a simple request
@@ -20,14 +22,31 @@ type FooResponse struct {
 	Bar string
 }
 
+type Request struct {
+	Action   string
+	Tagname  string
+	Response string
+}
+
 type Config struct {
-  Request Request[]
+	Requests []Request `toml:"request"`
 }
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Fprintln(os.Stderr, "usage:", os.Args[0], "/path.sock")
+		fmt.Fprintln(os.Stderr, "usage:", os.Args[0], "/path.sock config.toml")
 		return
+	}
+
+	configString, err := os.ReadFile(os.Args[2])
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	var config Config
+	if _, err := toml.Decode(string(configString), &config); err != nil {
+		fmt.Println(os.Stderr, "Unable to parse config")
 	}
 
 	fmt.Println("Unix SOAP server")
@@ -36,29 +55,30 @@ func main() {
 
 	soapServer := soap.NewServer()
 
-	soapServer.RegisterHandler(
-		"/",
-		// SOAPAction
-		"operationFoo",
-		// tagname of soap body content
-		"fooRequest",
-		// RequestFactoryFunc - give the server sth. to unmarshal the request into
-		func() interface{} {
-			return &FooRequest{}
-		},
-		// OperationHandlerFunc - do something
-		func(request interface{},
-			w http.ResponseWriter,
-			httpRequest *http.Request,
-		) (response interface{}, err error) {
-			fooRequest := request.(*FooRequest)
-			fooResponse := &FooResponse{
-				Bar: "Hellow " + fooRequest.Foo,
-			}
-			response = fooResponse
-			return
-		},
-	)
+	for _, r := range config.Requests {
+		fmt.Printf("Registering: %s %s\n", r.Action, r.Tagname)
+
+		soapServer.RegisterHandler(
+			"/",
+			// SOAPAction
+			r.Action,
+			// tagname of soap body content
+			r.Tagname,
+			// RequestFactoryFunc - give the server sth. to unmarshal the request into
+			func() interface{} {
+				return &FooRequest{}
+			},
+			// OperationHandlerFunc - do something
+			func(request interface{},
+				w http.ResponseWriter,
+				httpRequest *http.Request,
+			) (response interface{}, err error) {
+				response = r.Response
+				return
+			},
+		)
+
+	}
 
 	unixListener, err := net.Listen("unix", os.Args[1])
 	if err != nil {
